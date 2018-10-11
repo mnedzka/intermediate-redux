b732e71063fc2e812a48fc85b674343677354b30
Michal Nedzka <michael.nedzka@gmail.com>
2018-10-11 22:44:51 +0200

    Reorganize actions and reducers folders and import actions to dispatchers as properties of object

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const store = createStore(reducer);
-
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+const store = createStore(
+  burgerBuilderReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
