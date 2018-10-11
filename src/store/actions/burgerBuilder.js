6ab661a5f5514d8d19135871f36a73c8ada3da37
Michal Nedzka <michael.nedzka@gmail.com>
2018-10-11 23:54:30 +0200

    Add redux-thunk, new actions, moved fetch function

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
 
 export const addIngredient = name => {
   return {
@@ -11,5 +12,32 @@ export const removeIngredient = name => {
   return {
     type: actionTypes.REMOVE_INGREDIENT,
     ingredientName: name
+  };
+};
+
+export const setIngredients = ingredients => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    ingredients
+  };
+};
+
+export const fetchIngredientsFailed = ingredients => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED,
+    ingredients
+  };
+};
+
+export const initIngredients = name => {
+  return dispatch => {
+    axios
+      .get("https://react-my-burger.firebaseio.com/ingredients.json")
+      .then(response => {
+        dispatch(setIngredients(response.data));
+      })
+      .catch(error => {
+        dispatch(fetchIngredientsFailed());
+      });
   };
 };
